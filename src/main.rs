1770f19a5db08290ecf4d8019c81ddcb7ae4bd1b
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-04-28 11:56:20 +0200

    add a function that use pointers and deference them as unsafe code for learning purpose

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#[allow(unused)]
+fn pointers(){
+    let mut number:i64 =5;
+
+    let r1 =&number as *const i64;
+    let r2 =&mut number as *mut i64;
+
+    //let address =0x012345usize;
+    //let r = address as *const i64;
+
+    unsafe{
+        println!("r1 equal: {}",*r1);
+        println!("r1 equal: {}",*r2);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    pointers();
 }
